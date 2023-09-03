529dda82c62b95788dc4075aed677696a48fb8d9
Surbhit Sinha <surbhitsinha@Surbhits-MacBook-Air.local>
2023-09-04 02:24:31 +0530

    Adding coursal to the project

--- fooddelivery/src/screens/Home.js
+++ fooddelivery/src/screens/Home.js
@@ -2,12 +2,17 @@ import React from "react";
 import Card from "../components/Card";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import Carousal from "../components/Carousal";
 
 function Home() {
   return (
     <div>
       <div>
         <NavBar />
+      </div>
+
+      <div>
+        <Carousal />
       </div>
 
       <div>
