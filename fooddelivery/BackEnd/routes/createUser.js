4d862b1e89cf6d77ab37b9481192ad7897fe9dd0
Surbhit Sinha <surbhitsinha@Surbhits-MacBook-Air.local>
2023-09-15 02:44:40 +0530

    Adding the 1st API call

--- fooddelivery/BackEnd/routes/createUser.js
+++ fooddelivery/BackEnd/routes/createUser.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/createUser", async (req, res) => {
-  try {
-    await User.create({
-      name: req.body.name,
-      location: req.body.location,
-      email: req.body.email,
-      password: req.body.password,
-    });
+router.post(
+  "/createUser",
+  [
+    body("name", "Min Length of the user name can be 5").isLength({ min: 5 }),
+    body("email", "Incorrect Email format").isEmail(),
+    body("password", "Password to be 5 or more characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      await User.create({
+        name: req.body.name,
+        location: req.body.location,
+        email: req.body.email,
+        password: req.body.password,
+      });
 
-    res.json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.json({ success: false });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
   }
-});
+);
 
 module.exports = router;
